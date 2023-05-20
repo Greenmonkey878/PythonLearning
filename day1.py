3e8557ae3a90256d20ff3d1f0d0655cd8ab22ce3
Greenmonkey878 <96891123+Greenmonkey878@users.noreply.github.com>
2023-05-19 20:30:30 -0500

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,17 @@
+#============Exercise-1=================
+#name = input("What is your name? ")
+#length = len(name)
+#print(length)
+#============Exercise-2=================
+#a = input("a: ")
+#b = input("b: ")
+#temp = a
+#a = b
+#b = temp
+#print("a: " + a)
+#print("b: " + b) 
+#===========Exercise-3=================
+print("Welcome to the band name generator.\n")
+city = input ("What city did you grow up in?\n")
+pet_name = input("What is your pets name?\n")
+print("Your band name is " + city + " " + pet_name)
