42c6b63b9d9da68ad858baa8f9f6446ba5860b0a
Greenmonkey878 <96891123+Greenmonkey878@users.noreply.github.com>
2023-05-29 09:34:26 -0500

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,56 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+#Exercise==========One================
+
+number = int(input("Which number do you want to check? "))
+
+if number % 2 != 1:
+    print("This is an even number")
+else:
+    print("This is an odd number")
+
+#Exercise two
+weight =int(input("How much do you weight? "))
+height =int(input("How tall are you? "))
+
+bmi = round(weight / height ** 2)
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+    if bmi < 25:
+        print(f"Your BMI is {bmi} , you are normal weight.")
+    elif bmi < 30:
+        print(f"Your BMI is {bmi}, you are slighty over weight.")
+    elif bmi < 35:
+        print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
+
+#Exercise=============three=============
+year = int(input("Which year do you want to check"))
+
+if year % 4  == 0:
+    print("Leapyear")
+    if year % 100 == 0:
+        print("Leapyear")
+    else: 
+        print("not a leapyear")
+        if year % 400 == 0:
+          print("Leapyear")
+        else:
+            print("not a leapyear")
+else:
+    print("not a leapyear")
