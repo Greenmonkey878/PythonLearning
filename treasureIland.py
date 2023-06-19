b0cc23a511813a4a2366000102b8a542eb3f3c58
Greenmonkey878 <96891123+Greenmonkey878@users.noreply.github.com>
2023-06-19 16:43:26 -0500

    Add files via upload

---
+++ treasureIland.py
@@ -0,0 +1,15 @@
+print("Welcome to Treasure Island. Your mission is to find the treasure.")
+direction=input("Left or Right?")
+
+if direction == ("right"):
+    print("Game over.")
+else:direction == ("left")
+print("swim or wait")
+swim=input("swim")
+wait=input("wait")
+if swim==("swim"):
+    print("Game over")
+else: wait==("game over")
+print("Which door? Red, Yellow, Blue")
+
+door=input("red, yellow, blue")
